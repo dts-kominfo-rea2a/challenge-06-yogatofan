9989d8ca2ca9a6df93257da4c834fc5ecfdd8ae7
Yoga Ari Tofan <y.aritofan@gmail.com>
2022-06-14 13:49:05 +0700

    challenge: JS Callback 2

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,38 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  const result = [];
+
+  fs.readFile(file1, { encoding: "UTF-8" }, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    } else {
+      const dataResult = JSON.parse(data);
+      result.push(dataResult.message.split(" ")[1]);
+    }
+  });
+
+  fs.readFile(file2, { encoding: "UTF-8" }, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    } else {
+      const dataResult = JSON.parse(data);
+      result.push(dataResult[0].message.split(" ")[1]);
+    }
+  });
+
+  fs.readFile(file3, { encoding: "utf-8" }, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    } else {
+      const dataResult = JSON.parse(data);
+      result.push(dataResult[0].data.message.split(" ")[1]);
+
+      fnCallback(null, result);
+    }
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
